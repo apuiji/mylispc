cb10256f7587d0e0c29a3d2f5dcaa120adbd3dd5
apuiji <2330683684@qq.com>
2024-04-09 11:53:41 +0800

    a

--- mylisp/mylisp.hh
+++ mylisp/mylisp.hh
@@ -1,11 +1,34 @@
 #pragma once
 
+#include<list>
 #include<set>
 #include<string>
 
 namespace zlt::mylisp {
+  struct Object;
+
+  using Value = std::variant<std::monostate, double, char, const std::string *, Object *, void (*)(void *, void *)>;
+
+  struct Coroutine {
+    Value ax;
+    Value *bp;
+    Value *sp;
+    Value *tdsp;
+    const char *pc;
+    struct {
+      Value *data;
+      size_t size;
+    } valuek;
+  };
+
+  using Coroutines = std::list<Coroutine>;
+  using ItCoroutine = Coroutines::iterator;
+
+  extern Coroutines coroutines;
+  extern ItCoroutine itCoroutine;
   extern std::set<std::string> bodies;
   extern std::set<std::string> strings;
 
   const std::string *addString(std::string &&s);
+  void exec();
 }
