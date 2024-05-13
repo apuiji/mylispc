86f82988e72eacedc3664c55d8f8a1252b754934
apuiji <2330683684@qq.com>
2024-05-13 21:33:58 +0800

    a

--- preproc.c
+++ preproc.c
@@ -72,6 +72,7 @@ void **preprocList(void **dest, Context *ctx, void **src, void **first) {
     *next = *first;
     return preprocList(dest, ctx, src, first1);
   }
+  // TODO
   A:
   {
     void *first1 = NULL;
@@ -135,6 +136,8 @@ bool macroExpTreeMake1(MacroExpTree **dest, Context *ctx, const zltString **para
   return macroExpTreeMake1(dest, ctx, param + 1, param - 1, zltLinkMemb(src, next));
 }
 
+static bool clone(void **dest, Context *ctx, const void *src);
+static void **clones(void **dest, Context *ctx, const void *src);
 static bool cloneEOLAtom(void **dest, Context *ctx);
 static bool cloneIDAtom(void **dest, Context *ctx, const mylispcIDAtom *src);
 static bool cloneListAtom(void **dest, Context *ctx, const mylispcListAtom *src);
@@ -142,17 +145,130 @@ static bool cloneNumAtom(void **dest, Context *ctx, const mylispcNumAtom *src);
 static bool cloneStrAtom(void **dest, Context *ctx, const mylispcStrAtom *src);
 static bool cloneTokenAtom(void **dest, Context *ctx, const mylispcTokenAtom *src);
 
+static inline bool isDot3(const void *s) {
+  return zltStrMemb(s, size) >= 3 && !strncmp(zltStrMemb(s, data), "...", 3);
+}
+
 void **macroExpand1(void **dest, Context *ctx, const MacroExpTree *expTree, const void *src) {
   if (!src) {
     return dest;
   }
   int clazz = mylispcNodeMemb(src, clazz);
   if (clazz == MYLISPC_EOL_ATOM_CLASS) {
-    if (!cloneEOLAtom(dest)) {
+    if (!cloneEOLAtom(dest, ctx)) {
+      return NULL;
+    }
+  } else if (clazz == MYLISPC_ID_ATOM_CLASS) {
+    void *et = zltPtrTreeFind(expTree, mylispcIDAtomMemb(src, raw));
+    if (!et) {
+      if (!cloneIDAtom(dest, ctx, (const mylispcIDAtom *) src)) {
+        return NULL;
+      }
+    } else if (isDot3(zltPtrTreeMemb(et, value))) {
+      void **next = clones(dest, ctx, macroExpTreeMemb(et, node));
+      if (!next) {
+        return NULL;
+      }
+      return macroExpand1(next, ctx, expTree, zltLinkMemb(src, next));
+    } else {
+      if (!clone(dest, ctx, macroExpTreeMemb(et, node))) {
+        return NULL;
+      }
+    }
+  } else if (clazz == MYLISPC_LIST_ATOM_CLASS) {
+    mylispcListAtom *a = zltTypeAlloc(mylispcListAtom);
+    if (!a) {
+      mylispcReportBad(ctx, MYLISPC_OOM_BAD);
+      return NULL;
+    }
+    void *first = NULL;
+    if (!macroExpand1(&first, ctx, expTree, mylispcListAtomMemb(src, first))) {
+      mylispcNodeClean(first, NULL);
+      return false;
+    }
+    *a = mylispcListAtomMake(first);
+    *dest = a;
+  } else if (clazz == MYLISPC_NUM_ATOM_CLASS) {
+    if (!cloneNumAtom(dest, ctx, (const mylispcNumAtom *) src)) {
+      return NULL;
+    }
+  } else if (clazz == MYLISPC_STR_ATOM_CLASS) {
+    if (!cloneStrAtom(dest, ctx, (const mylispcStrAtom *) src)) {
+      return NULL;
+    }
+  } else {
+    if (!cloneTokenAtom(dest, ctx, (const mylispcTokenAtom *) src)) {
       return NULL;
     }
   }
+  return macroExpand1(&zltLinkMemb(*dest, next), ctx, expTree, zltLinkMemb(src, next));
 }
+
+bool clone(void **dest, Context *ctx, const void *src) {
+  int clazz = mylispcNodeMemb(src, clazz);
+  if (clazz == MYLISPC_EOL_ATOM_CLASS) {
+    if (!cloneEOLAtom(dest, ctx)) {
+      return false;
+    }
+  } else if (clazz == MYLISPC_ID_ATOM_CLASS) {
+    if (!cloneIDAtom(dest, ctx, (const mylispcIDAtom *) src)) {
+      return false;
+    }
+  } else if (clazz == MYLISPC_LIST_ATOM_CLASS) {
+    if (!cloneListAtom(dest, ctx, (const mylispcListAtom *) src)) {
+      return false;
+    }
+  } else if (clazz == MYLISPC_NUM_ATOM_CLASS) {
+    if (!cloneNumAtom(dest, ctx, (const mylispcNumAtom *) src)) {
+      return false;
+    }
+  } else if (clazz == MYLISPC_STR_ATOM_CLASS) {
+    if (!cloneStrAtom(dest, ctx, (const mylispcStrAtom *) src)) {
+      return false;
+    }
+  } else {
+    if (!cloneTokenAtom(dest, ctx, (const mylispcTokenAtom *) src)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void **clones(void **dest, Context *ctx, const void *src) {
+  if (!src) {
+    return dest;
+  }
+  if (!clone(dest, ctx, src)) {
+    return NULL;
+  }
+  return clones(&zltLinkMemb(*dest, next), ctx, zltLinkMemb(src, next));
+}
+
+bool cloneEOLAtom(void **dest, Context *ctx) {
+  mylispcEOLAtom *a = zltTypeAlloc(mylispcEOLAtom);
+  if (!a) {
+    mylispcReportBad(ctx, MYLISPC_OOM_BAD);
+    return false;
+  }
+  *dest = a;
+  return true;
+}
+
+bool cloneIDAtom(void **dest, Context *ctx, const mylispcIDAtom *src) {
+  mylispcIDAtom *a = zltTypeAlloc(mylispcIDAtom);
+  if (!a) {
+    mylispcReportBad(ctx, MYLISPC_OOM_BAD);
+    return false;
+  }
+  *dest = a;
+  return true;
+}
+
+bool cloneListAtom(void **dest, Context *ctx, const mylispcListAtom *src) {}
+
+static bool cloneNumAtom(void **dest, Context *ctx, const mylispcNumAtom *src);
+static bool cloneStrAtom(void **dest, Context *ctx, const mylispcStrAtom *src);
+static bool cloneTokenAtom(void **dest, Context *ctx, const mylispcTokenAtom *src);
 
 static bool cloneEmptyListAtom(void **dest);
 static bool cloneEOLAtom(void **dest);
@@ -309,36 +425,3 @@ void **clones(void **dest, const void *src) {
   }
   return clones(&zltMemb(*dest, zltLink, next), zltMemb(src, zltLink, next));
 }
-
-static void **macroExpand(
-  void **dest, mylispxPosStack *posk, mylispcMacroTree *macroTree, mylispcMacro *macro, const void *src);
-static void **preprocList1(void **dest, mylispxPosStack *posk, mylispcMacroTree *macroTree, const void *src);
-
-void **preprocList(void **dest, mylispxPosStack *posk, mylispcMacroTree *macroTree, const void *src) {
-  if (!src) {
-    return cloneEmptyListAtom(dest) ? dest : NULL;
-  }
-  int clazz = zltMemb(src, mylispcNode, clazz);
-  if (clazz == MYLISPC_EOL_ATOM_CLASS) {
-    cloneEOLAtom(dest);
-    ++posk->top->li;
-    return preprocList(&zltMemb(*dest, zltLink, next), posk, macroTree, zltMemb(src, zltLink, next));
-  }
-  if (clazz == MYLISPC_ID_ATOM_CLASS) {
-    mylispcMacroTree *mt = mylispcMacroTreeFind(macroTree, zltMemb(src, mylispcIDAtom, raw));
-    if (!mt) {
-      goto A;
-    }
-    return macroExpand(dest, posk, macroTree, &mt->macro, zltMemb(src, zltLink, next));
-  }
-  if (clazz == MYLISPC_LIST_ATOM_CLASS) {}
-  if (clazz == MYLISPC_NUM_ATOM_CLASS) {}
-  A:
-  return preprocList1(dest, posk, macroTree, src);
-}
-
-typedef struct {
-  zltRBTree rbTree;
-  zltString name;
-  void *atom;
-} MacroExpansionMap;
